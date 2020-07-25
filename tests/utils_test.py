fe391abf25b3127e37d562bbdd8b0b23061c0f43
Chan Tak Sum <chantaksum1999@gmail.com>
2020-07-26 01:24:36 +0800

    add feeds and unit test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,3 +6,9 @@ def test_isTradable():
     assert(utils.isTradable(dt.date(2020, 7, 2)) == True)
     assert(utils.isTradable(dt.date(2020, 7, 12)) == False)
     assert(utils.isTradable(dt.date(2020, 7, 13)) == True)
+
+def test_loadData():
+    assert(len(utils.loadData('388')) == 8)
+    assert(len(utils.loadData('388', '20200720')) == 5)
+    assert(len(utils.loadData('388', '', '20200724')) == 8)
+    assert(len(utils.loadData('388', '20200720', '20200724')) == 5)
