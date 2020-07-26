ad3bda2669c14067010e254af9429db8f8aabbd2
Chan Tak Sum <chantaksum1999@gmail.com>
2020-07-27 01:19:23 +0800

    misc

--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ from utils import isTradable
 from config import TICKERS, FEEDS_FOLDER, PARSING_TIME
 
 
-def run(tickers, feeds_folder = './feeds') -> None:
+def run(tickers, feeds_folder='./feeds') -> None:
     date = dt.date.today()
     if isTradable(date):
         for ticker in tickers:
@@ -18,9 +18,11 @@ def run(tickers, feeds_folder = './feeds') -> None:
                 try:
                     feeds_path = os.path.join(feeds_folder, ticker)
                     os.makedirs(feeds_path, exist_ok=True)
-                    r = requests.get(f'https://quote.ticker.com.hk/api/historical_data/detail/{ticker}/1d')
+                    r = requests.get(
+                        f'https://quote.ticker.com.hk/api/historical_data/detail/{ticker}/1d')
                     df = pd.DataFrame(r.json()['data'])
-                    df.to_csv(os.path.join(feeds_path, date.strftime('%Y%m%d')+'.csv'), index=False)
+                    df.to_csv(os.path.join(feeds_path, date.strftime(
+                        '%Y%m%d')+'.csv'), index=False)
                     gotcha = True
                 except:
                     print('retrying...')
